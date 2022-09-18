1691a4c29b9e7e9d44299092786180246b7cb52c
Dinz <kostan-82@yandex.ru>
2022-09-18 23:10:22 +0300

    1. Spring DI. Запуск [#2091]

--- src/main/java/ru/job4j/di/Main.java
+++ src/main/java/ru/job4j/di/Main.java
@@ -1,16 +1,16 @@
 package ru.job4j.di;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        Context context = new Context();
-        context.reg(Store.class);
-        context.reg(ConsoleInput.class);
-        context.reg(StartUI.class);
-
-        StartUI ui = context.get(StartUI.class);
-
-        ui.add("Petr Arsentev");
-        ui.add("Ivan ivanov");
-        ui.print();
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(Store.class);
+        context.register(ConsoleInput.class);
+        context.register(StartUI.class);
+        StartUI startUI = context.getBean(StartUI.class);
+        startUI.add("Petr Arsentev");
+        startUI.add("Ivan ivanov");
+        startUI.print();
     }
 }
